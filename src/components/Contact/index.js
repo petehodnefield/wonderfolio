5bcde32bb009c47931eb28055c341387adda2d07
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-07-16 08:51:39 -0500

    added form structure and styling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Contact() {
+    const [formState, setFormState] = useState({name: '', email: '', message: ''})
+    const {name, email, message} = formState
+
+
+    function handleFormSubmit(e) {
+        e.preventDefault()
+    }
+
     return (
-        <section className='yaya'>
-            <h2 className='section-header'>Contact Me</h2>
-            <form id='contact-form'>
-                <input>Your Name</input>
-                <label>Hi there</label>
-                <button type='submit'>Submit</button>
-            </form>
-        </section>
+        <section>
+        <h1>Contact Me</h1>
+        <form className='contact-form' id='contact-form' onSubmit={handleFormSubmit} >
+            <div className='form-element-container'>
+                <label htmlFor='name'>Name: </label>
+                <input className='form-input' type="text" name='name' defaultValue={name}/>
+            </div>
+            <div className='form-element-container'>
+                <label htmlFor='email'>Email Address: </label>
+                <input className='form-input' type="email"  name='email' defaultValue={email}/>
+            </div>
+            <div className='form-element-container'>
+                <label htmlFor='message'>Message: </label>
+                <textarea className='form-input' name='message'  rows="5" defaultValue={message}/>
+            </div>
+           
+            <button className='form-submit-btn' type='submit'>Submit</button>
+        </form>
+    </section>
     )
 }
 
